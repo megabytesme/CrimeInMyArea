22d76a41a5be611e386c50b8819ccaa0b894f641
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2025-05-08 23:19:10 +0100

    Added models (based on data.police.uk API)

--- Shared_Code/Models/Models.cs
+++ Shared_Code/Models/Models.cs
@@ -1,10 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Shared_Code.Models
+﻿namespace Shared_Code.Models
 {
-    internal class Models
+    public enum NotificationGranularity
     {
+        Street,
+        Neighbourhood,
+        Force,
+    }
+
+    public class CrimeReport
+    {
+        public string Summary { get; set; }
+        public int CrimeCount { get; set; }
+        public DateTime DataMonth { get; set; }
+        public NotificationGranularity Granularity { get; set; }
+        public string AreaIdentifier { get; set; }
+        public List<CrimeIncident> Incidents { get; set; }
+
+        public CrimeReport()
+        {
+            Incidents = new List<CrimeIncident>();
+            Summary = "No data available.";
+            CrimeCount = 0;
+        }
+    }
+
+    public class CrimeIncident
+    {
+        public string Category { get; set; }
+        public string LocationType { get; set; }
+        public Location Location { get; set; }
+        public string Context { get; set; }
+        public OutcomeStatus OutcomeStatus { get; set; }
+        public string PersistentId { get; set; }
+        public int Id { get; set; }
+        public string LocationSubtype { get; set; }
+        public string Month { get; set; }
+    }
+
+    public class Location
+    {
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public Street Street { get; set; }
+    }
+
+    public class Street
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class OutcomeStatus
+    {
+        public string Category { get; set; }
+        public string Date { get; set; }
+    }
+
+    public class AvailableDate
+    {
+        public string Date { get; set; }
+    }
+
+    public class NeighbourhoodLocation
+    {
+        public string Force { get; set; }
+        public string Neighbourhood { get; set; }
+    }
+
+    public class BoundaryPoint
+    {
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
     }
 }
